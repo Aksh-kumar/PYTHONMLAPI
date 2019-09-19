468efc235d8edbaf72e2c72c1bd6e1cd8cdcf23c
Akash Kumar <jkuaka3@jci.com>
2019-09-19 18:47:05 +0530

    model sperated

---
+++ cluster/EM/EM_Main.py
@@ -0,0 +1,119 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from functools import reduce
+
+class EM :
+    # get sum of all responsibilty for a particular cluster
+    # resp = nxk metrix i.e resp[number_of_label, nuber_of_cluster]
+    get_soft_counts = lambda resp : np.sum(resp, axis=0)
+    def __init__(self) :
+        pass
+    # compute ln(exp^e1+expe2....)
+    def _ln_sum_exp(self, Z):
+        return np.log(np.sum(np.exp(Z)))
+    # End
+    def get_log_likelihood(self, data, weights, means, covs):
+        n_clusters = len(means)
+        dimension = len(data[0]) 
+        sum_ln_exp = 0
+        for d in data:
+            Z = np.zeros(n_clusters)
+            for k in range(n_clusters):
+                # compute exponential term in multivariate_normal
+                delta = np.array(d) - means[k]
+                exponent_term = np.dot(delta.T, np.dot(np.linalg.pinv(covs[k]), delta))
+                # Compute loglikelihood contribution for this data point and this cluster
+                Z[k] += np.log(weights[k])
+                Z[k] -= 1/2. * (dimension * np.log(2*np.pi) + np.log(np.linalg.det(covs[k])) + exponent_term)
+                print(np.log(np.linalg.det(covs[k])))
+            # Increment loglikelihood contribution of this data point across all clusters
+            sum_ln_exp += self._ln_sum_exp(Z)
+        return sum_ln_exp
+    # End
+    # E step - get the responsibility if cluster parameter is given
+    def get_responsibilities(self, data, weights, means, covariances):
+        n_data = len(data)
+        n_clusters = len(means)
+        resp = np.zeros((n_data, n_clusters))
+        for i in range(n_data):
+            for k in range(n_clusters):
+                resp[i, k] = weights[k]* multivariate_normal.pdf(data[i],means[k],covariances[k],allow_singular=True)
+        # Add up responsibilities over each data point and normalize
+        row_sums = resp.sum(axis=1)[:, np.newaxis]
+        resp = resp / row_sums
+        return resp
+    # End
+    #M-step update the parameter if given cluster responsibilty is given
+    # get list of weights which represents how much each cluster represented over all data points
+    # parameter count list of sum of soft counts for all clusters
+    def _get_weights(self, counts):
+        n_clusters = len(counts)
+        sum_count = np.sum(counts)
+        weights = np.array(list(map(lambda k : counts[k]/sum_count, range(n_clusters))))
+        return weights
+    # End
+    def _get_means(self, data, resp, counts):
+        n_clusters = len(counts)
+        n_data = len(data)
+        means = np.zeros((n_clusters, len(data[0])))
+        for k in range(n_clusters):
+            weighted_sum = reduce(lambda x,i : x + resp[i,k]*data[i], range(n_data), 0)
+            means[k] = weighted_sum/counts[k]
+        return means
+    # End
+    # update covariances metrix from responsibity metrix, means and counts
+    def _get_covariances(self, data, resp, counts, means):
+        n_clusters = len(counts)
+        dimension = len(data[0])
+        n_data = len(data)
+        covariances = np.zeros((n_clusters, dimension, dimension))
+        for k in range(n_clusters):
+            weighted_sum = reduce(lambda x, i :x + resp[i,k] * np.outer((data[i]-means[k]), (data[i]-means[k]).T),
+                                range(n_data), np.zeros((dimension, dimension)))
+            covariances[k] = weighted_sum/counts[k]
+        return covariances
+    # End
+    # EM algorithm
+    def EM_from_parameter(self, data, init_means, init_covariances, init_weights, maxiter=1000, thresh=1e-4, verbose=True):
+        # Make copies of initial parameters
+        means = init_means[:]
+        covariances = init_covariances[:]
+        weights = init_weights[:]
+        # Infer dimensions of dataset and the number of clusters
+        n_data = len(data)
+        # dimension = len(data[0])
+        n_clusters = len(means)
+        # Initialize some useful variables
+        resp = np.zeros((n_data, n_clusters))
+        ll = self.get_log_likelihood(data, weights, means, covariances)
+        ll_list = [ll]
+        for it in range(maxiter):
+            if verbose and it % 5 == 0:
+                print("Iteration %s" % it)
+            # E-step: calculate responsibilities
+            resp = self.get_responsibilities(data, weights, means, covariances)
+            # M-step calculate cluster parameter
+            counts = EM.get_soft_counts(resp)
+            weights = self._get_weights(counts)
+            means = self._get_means(data, resp, counts)
+            covariances = self._get_covariances(data, resp, counts, means)
+            ll_new = self.get_log_likelihood(data, weights, means, covariances)
+            ll_list.append(ll_new)
+            if (ll_new - ll) < thresh and ll_new > -np.inf:
+                break
+            ll = ll_new
+        out = {'weights': weights, 'means': means, 'covariances': covariances, 'loglikelihood': ll_list, 'responsibility': resp}
+        return out
+    # End
+    def EM_initializer_from_responsibility(self, data, resp) :
+        counts = EM.get_soft_counts(resp)
+        weights = self._get_weights(counts)
+        means = self._get_means(data, resp, counts)
+        covariances = self._get_covariances(data, resp, counts, means)
+        return means, covariances, weights
+    # End
+    def em(self, data, resp, max_iter = 1000, threshold = 1e-4) :
+        means, covariances, weights = self.EM_initializer_from_responsibility(data, resp)
+        return self.EM_from_parameter(data, means, covariances, weights, maxiter=max_iter, thresh=threshold)
+# End class
+
--- cluster/EM/em_business.py
+++ cluster/EM/em_business.py
@@ -1,4 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import imageio
+import os
+import pandas as pd
+import numpy as np
+from K_mean import K_Mean
+from EM_Main import EM
 class EMBusiness :
-    def __init__(self) :
-        pass
-# End
+    def __init__(self, path=None) :
+        self._dataset = None
+        self._X = None
+        self._k = None
+        self.init_centroids = None
+        self.init_cluster_assignment = None
+        self.df = pd.DataFrame([],columns=['Image_Name', 'Path', 'Extension', 'R', 'G', 'B' ])
+        self.image_EXT = ['.png', '.jpeg', '.jpg', '.jif', '.jpe']
+        self._km = K_Mean()
+        self._em = EM()
+        if path is not None :
+            self._dataset = self.get_dataset_from_path(path)
+            self.get_RGB_numpy_array()
+    # End
+    @property
+    def IMAGE_EXT_SUPPORTED(self) :
+        return self.image_EXT
+    @property
+    def dataset(self) :
+        return self._dataset.copy() if self._dataset is not None else None
+    # End
+    @dataset.setter
+    def dataset(self, value) :
+        self._dataset = value
+        self.get_RGB_numpy_array()
+    # End
+    @property
+    def k(self) :
+        return self._k
+    @k.setter
+    def k(self, value) :
+        try :
+            self._k = int(value)
+        except :
+            raise Exception('value must be Integer')
+    # End
+    def _get_image_feature(self, path) :
+        try :
+            Im = imageio.imread(os.path.join(path), pilmode='RGB')
+            temp = Im/255. # divide by 255 to get in fraction
+            mn = temp.sum(axis=0).sum(axis=0)/(temp.shape[0]*temp.shape[1])
+            mn = mn/sum(mn) # standardized the data
+            return mn
+        except :
+            return None
+    # End
+    def _create_image_dataset(self, path, df = None) :
+        dataset = self.df if df is None else df
+        for files in os.listdir(os.path.join(path)) :
+            f_path = os.path.join(path, files)
+            if os.path.isfile(f_path) :
+                temp = os.path.splitext(files)
+                ext = temp[1]
+                if str.lower(ext) in self.image_EXT :
+                    f_name = temp[0]
+                    mn = self._get_image_feature(f_path)
+                    #     file_name, path,extension,red, green, blue
+                    data = [f_name, f_path, ext, mn[0], mn[1], mn[2]]
+                    data_dic = dict(zip(list(dataset.columns), data))
+                    if df is not None :
+                        dataset = dataset.append(data_dic, ignore_index=True)
+            if os.path.isdir(f_path) :
+                dataset = self._create_image_dataset(f_path , dataset)
+        return dataset
+    # End
+    def get_RGB_numpy_array(self, dataset=None) :
+        X = dataset.iloc[:, [3, 4, 5]].values if dataset is not None else self._dataset.iloc[:, [3, 4, 5]].values
+        if dataset is None :
+            self._X = X[:, :]    
+        return X
+    # End
+    def get_dataset_from_path(self, path, num_rows = None, in_dic_list = False) :
+        self._dataset = self._create_image_dataset(path)
+        if in_dic_list :
+            return self.dataset.T.to_dict().values() if num_rows is None else self.dataset.head(num_rows).T.to_dict().values()
+        else :
+            return self.dataset if num_rows is None else self.dataset.head(num_rows)
+    # End
+    # parameter n is number of clusters with there associate heterogeneity and data is numpy array
+    def get_first_n_heterogeneity(self, n, data=None) :
+        data = data if data is not None else self._dataset
+        if data is None :
+            raise Exception('No data found')
+        X = self._X if data is None else data
+        return self._km.get_initial_k(X, n)
+    # End
+    # since cluster assignment produces hard assignment through k mean this fur computational
+    # stability we added small amount of reponsibility to not assigned cluster because for EM
+    # we have to take inverse of covariance metrix if it become a singular metrix it will not 
+    # get inverted
+    def get_stable_responsibilities(self, resp, k=None) :
+        k = k if k is not None else self._k
+        # Inner function
+        def get_soft_assignment_list(ls, n) :
+            temp = np.random.uniform(low=0.1, high=0.16,size =(n,))
+            index_1 = int(np.where(ls==1)[0])
+            sum_ele = temp.sum()
+            return np.insert(temp, index_1, 1-sum_ele)
+        return np.array(list(map(lambda y : get_soft_assignment_list(y, k-1), resp)))
+    # End
+    # expected parameter k- number of cluster and data is numpy array of feature is optional if
+    # not given then the dataset created through this object  RBG value is getting fetched
+    def get_initial_centroids_and_cluster_assignment(self, k=None, data=None, seed = None) :
+        data_temp = data if data is not None else self._X
+        k = k if k is not None else self._k
+        if data_temp is None :
+            raise Exception('No data found please use get_RGB_numpy_array(data(pandas dataframe)) to get RGB numpy array')
+        if k is None :
+            raise Exception('No value of k found please set value of k i.e object.k = value')
+        centroids, cluster_assignments = self._km.kmeans(data_temp, k, seed = seed)
+        if data is None and self._X is not None :
+            self.init_centroids = centroids
+            self.init_cluster_assignment = np.eye(k)[cluster_assignments]
+        return centroids, np.eye(k)[cluster_assignments]
+    # End
+    # parameter data numpy array, responsibility numpy list of responsibility, maxiteer maximum iteration of
+    # EM algorithm threshold - stopping threshold value if value not change further this
+    def em_params(self, data=None, resp=None, maxiter = 1000, thresh=1e-4) :
+        if data is None and self._X is None :
+            raise Exception('No data found')
+        elif data is None and self._X is not None :
+            if self.init_cluster_assignment is None :
+                raise Exception('call get_initial_centroids_and_cluster_assignment to set initial cluster assignment through k means')
+            resp = self.get_stable_responsibilities(self.init_cluster_assignment)
+            print(resp)
+            return self._em.em(self._X, resp, max_iter=maxiter, threshold=thresh)
+        elif data is not None and self._X is None :
+            return self._em.em(data, resp, max_iter=maxiter, threshold=thresh)
+        else :
+            return None
+    # End
+# End class
+
+if __name__ == '__main__' :
+    path = r'.\images'
+    emb = EMBusiness(path)
+    #res = emb.get_first_n_heterogeneity(55)
+    emb.k = 3
+    c, cl = emb.get_initial_centroids_and_cluster_assignment(seed = 0)
+    #print(c)
+    #print(cl)
+    res = emb.em_params()
+    print(res)
